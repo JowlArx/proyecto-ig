10f47d889a36b3ba8392b86d0970c3b0e92a2fb3
facufrias789 <130002637+facufrias789@users.noreply.github.com>
2023-05-15 15:34:04 -0300

    no anda wachooo

--- App.js
+++ App.js
@@ -1,132 +1,48 @@
-import React, { useState } from "react";
-import { Text, View, StyleSheet, Image, Button, Alert, TouchableOpacity, Platform, } from 'react-native';
-import Nashe from './assets/nashe.png';
-import * as ImagePicker from 'expo-image-picker';
-import * as Sharing from 'expo-sharing';
-import uploadAnonymousFilesAsync from "anonymous-files";
+import react from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert}  from 'react-native';
+import * as ImagePicker from "expo-image-picker";
+import {AntDesign } from "react-native-vector-icons/AntDesign";
+import { AntDesign} from "react-native-vector-icons";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Icon from 'ant-design/icons/lib/components/AntIcon';
 
-const App = () => {
+const App = ( )=>{
 
-
-  const [SelectedImg, SetSelectedImg] = useState(null)
-
-  const openShareDialog = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert("No podes compartir en esta plataforma");
-      return;
-    }
-
-    await Sharing.shareAsync(SelectedImg.localUri);
-
-  }
-
-  let OpenImagePicker = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if (permissionResult.granted === false) {
-      alert('necesitamos permisos para acceder a la galeria');
-      return;
-    }
-    const PickerResult = await ImagePicker.launchImageLibraryAsync()
-    console.log(PickerResult)
-    if (PickerResult.canceled === true) {
-      return;
-    }
-
-
-    if (Platform.OS === "web") {
-      let remoteUri = await uploadAnonymousFilesAsync(PickerResult.uri);
-      setSelectedImage({ localUri: PickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: PickerResult.uri });
+  let openImagePickerAsync = async()=>{
+    let permissionresult= await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (permissionresult.granted===false){
+    alert("los permisos para acceder a la camara son requeeridos")
+    return;
     }
   }
-  return (
-    <View style={styles.ViewHello}>
-      <Text>Hello World</Text>
-      <Image
-        source={Nashe}
-        style={styles.Image}
-      />
-      <TouchableOpacity onPress={OpenImagePicker}>
-        <Image
-          source={{
-            uri: SelectedImg !== null
-              ? SelectedImg.localUri
-              : "https://picsum.photos/200/2020",
-          }}
-          style={styles.Imagenashe}
-        />
-      </TouchableOpacity>
-
-      <Button
-        color="black"
-        title="botonaso"
-        onPress={() => console.log('Helow')}
-      />
-      <Text>separao por botonaso</Text>
-      <Button
-        color="black"
-        title="botonaso con alerta"
-        onPress={() => Alert.alert('Helow')}
-      />
-      <Text>separao por botonasoide</Text>
-
-      <TouchableOpacity onPress={() => Alert.alert('Holaaaa')} style={styles.Botonasoide}>
-        <Text style={styles.Botonasoidetxt}>botonasoide</Text>
-      </TouchableOpacity>
-      <Text>Otro botonaso</Text>
-
-      {
-        SelectedImg ? (
-          <TouchableOpacity onPress={openShareDialog} style={styles.Botonasoideimg}>
-            <Text style={styles.Botonasoidetxt}>botonaso pero pa compartir</Text>
-          </TouchableOpacity>
-          ) : (
-          <View />
-      )}
-
-    </View>
+  return(
+  <View  style={styles.container}>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />   
+   <Text style= {styles.title}>Hello word
+   </Text>
+   <AntDesign style={styles.container} name= "RightOutlined"  size={30} />
+  
+   
+   <Image
+   source={{uri: 'https://picsum.photos/200/200'}}
+   style={styles.image}
+   />
+   <TouchableOpacity
+   onPress={openImagePickerAsync}
+   style={styles.button}
+   >
+    <Text style={styles.buttontext} >Tocame</Text>
+   </TouchableOpacity>
+   </View> 
   );
 };
-
 const styles = StyleSheet.create({
-  ViewHello: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#757575"
-  },
-  Image: {
-    width: 200,
-    height: 200,
-  },
-  Imagenashe: {
-    width: 300,
-    height: 300,
-    borderRadius: 100,
-
-  },
-  Botonasoide: {
-    width: 110,
-    height: 30,
-    alignItems: "center",
-    backgroundColor: "black",
-    borderRadius: 15,
-  },
-  Botonasoidetxt: {
-    flex: 1,
-    fontStyle: "italic",
-    textAlign: "center",
-    fontSize: 17,
-    color: "white"
-  },
-  Botonasoideimg: {
-    width: 230,
-    height: 30,
-    alignItems: "center",
-    backgroundColor: "black",
-    borderRadius: 15,
-  },
+  container:{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#292929"},
+  title : {fontSize: 30, color:"#dc143c"},
+  image:{height:200, width:200, borderRadius:100},
+  button:{backgroundColor:"red", padding: 7, marginTop: 10 }, 
+  buttontext: {color:  "blue"
+},
 });
 
 export default App;
