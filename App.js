afd044ddbd386141ef0e04cc870936bac5807eb0
facufrias789 <130002637+facufrias789@users.noreply.github.com>
2023-05-17 12:48:20 -0300

    listorti lo di todo loco
    quedo masomenos encontre los iconos masomenos parecidos

--- App.js
+++ App.js
@@ -1,48 +1,57 @@
-import react from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, Alert}  from 'react-native';
-import * as ImagePicker from "expo-image-picker";
-import {AntDesign } from "react-native-vector-icons/AntDesign";
-import { AntDesign} from "react-native-vector-icons";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import Icon from 'ant-design/icons/lib/components/AntIcon';
-
-const App = ( )=>{
-
-  let openImagePickerAsync = async()=>{
-    let permissionresult= await ImagePicker.requestMediaLibraryPermissionsAsync()
-    if (permissionresult.granted===false){
-    alert("los permisos para acceder a la camara son requeeridos")
-    return;
-    }
-  }
-  return(
-  <View  style={styles.container}>
-      <Ionicons name="md-checkmark-circle" size={32} color="green" />   
-   <Text style= {styles.title}>Hello word
-   </Text>
-   <AntDesign style={styles.container} name= "RightOutlined"  size={30} />
+import React from "react";
+import {
   
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Ionicons from "react-native-vector-icons/Ionicons";
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    <View style={styles.icons}>
+     
+      
+    <Ionicons name="home" size={30} color="white"/>
+     <Ionicons name="search-outline" size={30} color="white"/>
+     <Ionicons name="add-circle-outline" size={30} color="white"/>
+    <Ionicons name="film-outline" size={30} color="white"/>
    
-   <Image
-   source={{uri: 'https://picsum.photos/200/200'}}
-   style={styles.image}
-   />
-   <TouchableOpacity
-   onPress={openImagePickerAsync}
-   style={styles.button}
-   >
-    <Text style={styles.buttontext} >Tocame</Text>
-   </TouchableOpacity>
-   </View> 
+    <Ionicons name="ellipse-outline" size={32} color="white"/>
+
+
+
+      </View>
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
-  container:{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#292929"},
-  title : {fontSize: 30, color:"#dc143c"},
-  image:{height:200, width:200, borderRadius:100},
-  button:{backgroundColor:"red", padding: 7, marginTop: 10 }, 
-  buttontext: {color:  "blue"
-},
+  container: {
+    marginBottom: 10,
+    flex: 1,
+    
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "black",
+  },
+  icons: {
+    flexWrap: 'wrap', 
+    alignItems: "flex-end",
+    flexDirection:'row',
+    columnGap: 45,
+    marginTop: 100,
+    
+    position: "relative"
+  },
+  title: { fontSize: 30, color: "#dc143c" },
+  image: { height: 200, width: 200, borderRadius: 100 },
+  button: { backgroundColor: "red", padding: 7, marginTop: 10 },
+  buttontext: { color: "blue" },
 });
 
 export default App;
